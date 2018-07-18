cae679ae687aa9a69c715bc27d81fd3fe8dcf568
kirilcvetkov92 <kirilcvetkov92@gmail.com>
2018-07-18 05:02:45 +0200

    Updated YoloV3 conversion script

--- YoloV3/script.py
+++ YoloV3/script.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 """
-Reads Darknet53 config and weights and creates Keras model with TF backend.
-Currently only supports layers in Darknet53 config.
+Reads Darknet config and weights and creates Keras model with TF backend.
+
 """
 
 import argparse
@@ -12,18 +12,16 @@ from collections import defaultdict
 
 import numpy as np
 from keras import backend as K
-from keras.layers import (Conv2D, GlobalAveragePooling2D, Input, Reshape,
-                          ZeroPadding2D, UpSampling2D, Activation)
+from keras.layers import (Conv2D, Input, ZeroPadding2D, Add,
+                          UpSampling2D, MaxPooling2D, Concatenate)
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.merge import concatenate, add
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 from keras.regularizers import l2
 from keras.utils.vis_utils import plot_model as plot
 
 
-parser = argparse.ArgumentParser(
-    description='Yet Another Darknet To Keras Converter.')
+parser = argparse.ArgumentParser(description='Darknet To Keras Converter.')
 parser.add_argument('config_path', help='Path to Darknet cfg file.')
 parser.add_argument('weights_path', help='Path to Darknet weights file.')
 parser.add_argument('output_path', help='Path to output Keras model file.')
@@ -33,15 +31,14 @@ parser.add_argument(
     help='Plot generated Keras model and save as image.',
     action='store_true')
 parser.add_argument(
-    '-flcl',
-    '--fully_convolutional',
-    help='Model is fully convolutional so set input shape to (None, None, 3). '
-    'WARNING: This experimental option does not work properly for YOLO_v2.',
+    '-w',
+    '--weights_only',
+    help='Save as Keras weights file instead of model file.',
     action='store_true')
-
 
 def unique_config_sections(config_file):
     """Convert all config sections to have unique names.
+
     Adds unique suffixes to config sections for compability with configparser.
     """
     section_counters = defaultdict(int)
@@ -57,7 +54,7 @@ def unique_config_sections(config_file):
     output_stream.seek(0)
     return output_stream
 
-
+# %%
 def _main(args):
     config_path = os.path.expanduser(args.config_path)
     weights_path = os.path.expanduser(args.weights_path)
@@ -74,11 +71,13 @@ def _main(args):
     # Load weights and config.
     print('Loading weights.')
     weights_file = open(weights_path, 'rb')
-    weights_header = np.ndarray(
-        shape=(5, ), dtype='int32', buffer=weights_file.read(20))
-    print('Weights Header: ', weights_header)
-    # TODO: Check transpose flag when implementing fully connected layers.
-    # transpose = (weight_header[0] > 1000) or (weight_header[1] > 1000)
+    major, minor, revision = np.ndarray(
+        shape=(3, ), dtype='int32', buffer=weights_file.read(12))
+    if (major*10+minor)>=2 and major<1000 and minor<1000:
+        seen = np.ndarray(shape=(1,), dtype='int64', buffer=weights_file.read(8))
+    else:
+        seen = np.ndarray(shape=(1,), dtype='int32', buffer=weights_file.read(4))
+    print('Weights Header: ', major, minor, revision, seen)
 
     print('Parsing Darknet config.')
     unique_config_file = unique_config_sections(config_path)
@@ -86,20 +85,14 @@ def _main(args):
     cfg_parser.read_file(unique_config_file)
 
     print('Creating Keras model.')
-    if args.fully_convolutional:
-        image_height, image_width = None, None
-    else:
-        image_height = int(cfg_parser['net_0']['height'])
-        image_width = int(cfg_parser['net_0']['width'])
-
-    prev_layer = Input(shape=(image_height, image_width, 3))
-    all_layers = [prev_layer]
-    outputs = []
+    input_layer = Input(shape=(None, None, 3))
+    prev_layer = input_layer
+    all_layers = []
 
     weight_decay = float(cfg_parser['net_0']['decay']
                          ) if 'net_0' in cfg_parser.sections() else 5e-4
     count = 0
-
+    out_index = []
     for section in cfg_parser.sections():
         print('Parsing section {}'.format(section))
         if section.startswith('convolutional'):
@@ -110,12 +103,13 @@ def _main(args):
             activation = cfg_parser[section]['activation']
             batch_normalize = 'batch_normalize' in cfg_parser[section]
 
+            padding = 'same' if pad == 1 and stride == 1 else 'valid'
+
             # Setting weights.
             # Darknet serializes convolutional weights as:
             # [bias/beta, [gamma, mean, variance], conv_weights]
             prev_layer_shape = K.int_shape(prev_layer)
 
-            # TODO: This assumes channel last dim_ordering.
             weights_shape = (size, size, prev_layer_shape[-1], filters)
             darknet_w_shape = (filters, weights_shape[2], size, size)
             weights_size = np.product(weights_shape)
@@ -136,8 +130,6 @@ def _main(args):
                     buffer=weights_file.read(filters * 12))
                 count += 3 * filters
 
-                # TODO: Keras BatchNormalization mistakenly refers to var
-                # as std.
                 bn_weight_list = [
                     bn_weights[0],  # scale gamma
                     conv_bias,  # shift beta
@@ -155,7 +147,6 @@ def _main(args):
             # (out_dim, in_dim, height, width)
             # We would like to set these to Tensorflow order:
             # (height, width, in_dim, out_dim)
-            # TODO: Add check for Theano dim ordering.
             conv_weights = np.transpose(conv_weights, [2, 3, 1, 0])
             conv_weights = [conv_weights] if batch_normalize else [
                 conv_weights, conv_bias
@@ -170,12 +161,10 @@ def _main(args):
                     'Unknown activation function `{}` in section {}'.format(
                         activation, section))
 
-            padding = 'same' if pad == 1 and stride == 1 else 'valid'
-            # Adjust padding model for darknet.
-            if stride == 2:
-                prev_layer = ZeroPadding2D(((1, 0), (1, 0)))(prev_layer)
-
             # Create Conv2D layer
+            if stride>1:
+                # Darknet uses left and top padding instead of 'same' mode
+                prev_layer = ZeroPadding2D(((1,0),(1,0)))(prev_layer)
             conv_layer = (Conv2D(
                 filters, (size, size),
                 strides=(stride, stride),
@@ -188,7 +177,6 @@ def _main(args):
             if batch_normalize:
                 conv_layer = (BatchNormalization(
                     weights=bn_weight_list))(conv_layer)
-
             prev_layer = conv_layer
 
             if activation == 'linear':
@@ -198,19 +186,12 @@ def _main(args):
                 prev_layer = act_layer
                 all_layers.append(act_layer)
 
-        elif section.startswith('avgpool'):
-            if cfg_parser.items(section) != []:
-                raise ValueError('{} with params unsupported.'.format(section))
-            all_layers.append(GlobalAveragePooling2D()(prev_layer))
-            prev_layer = all_layers[-1]
-
         elif section.startswith('route'):
             ids = [int(i) for i in cfg_parser[section]['layers'].split(',')]
             layers = [all_layers[i] for i in ids]
-
             if len(layers) > 1:
                 print('Concatenating route layers:', layers)
-                concatenate_layer = concatenate(layers)
+                concatenate_layer = Concatenate()(layers)
                 all_layers.append(concatenate_layer)
                 prev_layer = concatenate_layer
             else:
@@ -218,46 +199,52 @@ def _main(args):
                 all_layers.append(skip_layer)
                 prev_layer = skip_layer
 
+        elif section.startswith('maxpool'):
+            size = int(cfg_parser[section]['size'])
+            stride = int(cfg_parser[section]['stride'])
+            all_layers.append(
+                MaxPooling2D(
+                    pool_size=(size, size),
+                    strides=(stride, stride),
+                    padding='same')(prev_layer))
+            prev_layer = all_layers[-1]
+
         elif section.startswith('shortcut'):
-            ids = [int(i) for i in cfg_parser[section]['from'].split(',')][0]
+            index = int(cfg_parser[section]['from'])
             activation = cfg_parser[section]['activation']
-            shortcut = add([all_layers[ids], prev_layer])
-            if activation == 'linear':
-                shortcut = Activation('linear')(shortcut)
-            all_layers.append(shortcut)
+            assert activation == 'linear', 'Only linear activation supported.'
+            all_layers.append(Add()([all_layers[index], prev_layer]))
             prev_layer = all_layers[-1]
 
         elif section.startswith('upsample'):
             stride = int(cfg_parser[section]['stride'])
-            all_layers.append(
-                UpSampling2D(
-                    size=(stride, stride))(prev_layer))
+            assert stride == 2, 'Only stride=2 supported.'
+            all_layers.append(UpSampling2D(stride)(prev_layer))
             prev_layer = all_layers[-1]
 
         elif section.startswith('yolo'):
-            classes = int(cfg_parser[section]['classes'])
-            # num = int(cfg_parser[section]['num'])
-            # mask = int(cfg_parser[section]['mask'])
-            n1, n2 = int(prev_layer.shape[1]), int(prev_layer.shape[2])
-            n3 = 3
-            n4 = (4 + 1 + classes)
-            yolo = Reshape((n1, n2, n3, n4))(prev_layer)
-            all_layers.append(yolo)
+            out_index.append(len(all_layers)-1)
+            all_layers.append(None)
             prev_layer = all_layers[-1]
-            outputs.append(len(all_layers) - 1)
 
-        elif (section.startswith('net')):
-            pass  # Configs not currently handled during model definition.
+        elif section.startswith('net'):
+            pass
+
         else:
             raise ValueError(
                 'Unsupported section header type: {}'.format(section))
 
     # Create and save model.
-    model = Model(inputs=all_layers[0],
-                  outputs=[all_layers[i] for i in outputs])
+    if len(out_index)==0: out_index.append(len(all_layers)-1)
+    model = Model(inputs=input_layer, outputs=[all_layers[i] for i in out_index])
     print(model.summary())
-    model.save('{}'.format(output_path))
-    print('Saved Keras model to {}'.format(output_path))
+    if args.weights_only:
+        model.save_weights('{}'.format(output_path))
+        print('Saved Keras weights to {}'.format(output_path))
+    else:
+        model.save('{}'.format(output_path))
+        print('Saved Keras model to {}'.format(output_path))
+
     # Check to see if all weights have been read.
     remaining_weights = len(weights_file.read()) / 4
     weights_file.close()
